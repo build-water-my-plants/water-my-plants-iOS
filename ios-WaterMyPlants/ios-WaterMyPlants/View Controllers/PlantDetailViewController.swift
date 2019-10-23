60574d492944418d4f73b3939d01e4a5c091e266
Michael R.F <mrflowers22@uwalumni.com>
2019-10-23 17:33:50 -0600

    added speciesTextField to  plantDetailViewController

--- ios-WaterMyPlants/ios-WaterMyPlants/View Controllers/PlantDetailViewController.swift
+++ ios-WaterMyPlants/ios-WaterMyPlants/View Controllers/PlantDetailViewController.swift
@@ -1,127 +1,128 @@
 //
 //  PlantDetailViewController.swift
-//  waterMyPlantTemp
+//  ios-WaterMyPlants
 //
-//  Created by Michael Flowers on 10/18/19.
-//  Copyright © 2019 Michael Flowers. All rights reserved.
+//  Created by Michael Flowers on 10/23/19.
+//  Copyright © 2019 LambdaSchool. All rights reserved.
 //
 
 import UIKit
 
 class PlantDetailViewController: UIViewController {
     var plant: Plant? {
-        didSet {
+            didSet {
+                updateViews()
+            }
+        }
+        var user: User? {
+            didSet {
+                updateViews()
+            }
+        }
+        
+        var imagePicker = UIImagePickerController()
+        
+        @IBOutlet weak var plantImageView: UIImageView!
+        @IBOutlet weak var plantNameTextField: UITextField!
+        @IBOutlet weak var datePickerProperties: UIDatePicker!
+        @IBOutlet weak var speciesTextField: UITextField!
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
             updateViews()
+            imagePicker.delegate = self
         }
-    }
-    var owner: Owner? {
-        didSet {
-            updateViews()
+        
+        func repeatSchedule(day: Int, hour: Int, min: Int ){
+           
         }
-    }
-    
-    var imagePicker = UIImagePickerController()
-    
-    @IBOutlet weak var plantImageView: UIImageView!
-    @IBOutlet weak var plantNameTextField: UITextField!
-    @IBOutlet weak var datePickerProperties: UIDatePicker!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateViews()
-        imagePicker.delegate = self
-    }
-    
-    func repeatSchedule(day: Int, hour: Int, min: Int ){
-       
-     
-    }
-    
-    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
-        guard let image = plantImageView.image, let name = plantNameTextField.text, !name.isEmpty else { return }
-        let data = image.jpegData(compressionQuality: 0.5)
-      
-        if let passedInPlant = plant {
-            //UPDATE PLANT
-             print(" this is the date string format: \(datePickerProperties.date.formatDate())")
-            PlantController.update(plant: passedInPlant, withNewName: name, withNewSchedule: datePickerProperties.date, withNewPhoto: data)
-        }  else  {
-            //CREATE NEW PLANT
-//            guard let passedInOwner = owner else { return }
-            let owner = Owner(username: "testOwner1", phoneNumber: "owner1@email.com", password: "password")
-            print(" this is the date string format: \(datePickerProperties.date.formatDate())")
-            PlantController.createPlantWith(name: name, photo: data, schedule: datePickerProperties.date, owner: owner)
+        
+        @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+            guard let image = plantImageView.image, let name = plantNameTextField.text, !name.isEmpty, let speciesName = speciesTextField.text, !speciesName.isEmpty else { return }
+            let data = image.jpegData(compressionQuality: 0.5)
+          
+            if let passedInPlant = plant {
+                //UPDATE PLANT
+                 print(" this is the date string format: \(datePickerProperties.date.formatDate())")
+                PlantController.update(plant: passedInPlant, withNewName: name, withNewSpecies: speciesName, withNewSchedule: datePickerProperties.date, withNewPhoto: data)
+            }  else  {
+                //CREATE NEW PLANT
+    //            guard let passedInOwner = owner else { return }
+                let user = User(username: "testOwner1", phoneNumber: "owner1@email.com", password: "password")
+                print(" this is the date string format: \(datePickerProperties.date.formatDate())")
+                PlantController.createPlantWith(name: name, species: speciesName, photo: data, schedule: datePickerProperties.date, user: user)
+            }
+            navigationController?.popToRootViewController(animated: true)
         }
-        navigationController?.popToRootViewController(animated: true)
-    }
-    
-    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
-        //SHOW ACTION SHEET WI/OPTION TO CHOOSE LIBRARY OR CAMERA
-        actionSheet()
-    }
-    
-    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
-        //???
-    }
-    
-    private func updateViews(){
-        guard let passedInPlant = plant, let photoData = passedInPlant.photo, let photoImage = UIImage(data: photoData), let date = passedInPlant.schedule, isViewLoaded else {
-            title = "Please create plant"
-            return }
-        plantImageView.image = photoImage
-        plantNameTextField.text = passedInPlant.name
-        datePickerProperties.date = date
+        
+        @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+            //SHOW ACTION SHEET WI/OPTION TO CHOOSE LIBRARY OR CAMERA
+            actionSheet()
+        }
+        
+        @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+            //???
+        }
+        
+        private func updateViews(){
+            guard let passedInPlant = plant, let photoData = passedInPlant.photo, let photoImage = UIImage(data: photoData), let date = passedInPlant.schedule, isViewLoaded else {
+                title = "Please create plant"
+                return }
+            plantImageView.image = photoImage
+            plantNameTextField.text = passedInPlant.name
+            datePickerProperties.date = date
+        }
+
+        func showCamera(){
+            //check to see if they have camera
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                //dress up camera
+                imagePicker.sourceType = .camera
+                imagePicker.allowsEditing = false
+                //present the imagepicker
+                present(imagePicker, animated: true)
+            } else {
+                //show library
+            }
+        }
+        
+        func showLibrary(){
+            //check to see if library is available
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.allowsEditing = false
+                present(imagePicker, animated: true)
+            }
+        }
+        
+        func actionSheet(){
+            let alert = UIAlertController(title: "Chose Camera or Photo Library", message: nil, preferredStyle: .actionSheet)
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+                self.showCamera()
+            }
+            
+            let libraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+                self.showLibrary()
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            alert.addAction(cameraAction)
+            alert.addAction(libraryAction)
+            alert.addAction(cancelAction)
+            present(alert, animated: true)
+        }
     }
 
-    func showCamera(){
-        //check to see if they have camera
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            //dress up camera
-            imagePicker.sourceType = .camera
-            imagePicker.allowsEditing = false
-            //present the imagepicker
-            present(imagePicker, animated: true)
-        } else {
-            //show library
-        }
-    }
-    
-    func showLibrary(){
-        //check to see if library is available
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.allowsEditing = false
-            present(imagePicker, animated: true)
-        }
-    }
-    
-    func actionSheet(){
-        let alert = UIAlertController(title: "Chose Camera or Photo Library", message: nil, preferredStyle: .actionSheet)
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
-            self.showCamera()
+    extension PlantDetailViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            self.plantImageView.image = image
+            
+            dismiss(animated: true)
         }
         
-        let libraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
-            self.showLibrary()
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            dismiss(animated: true)
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        
-        alert.addAction(cameraAction)
-        alert.addAction(libraryAction)
-        alert.addAction(cancelAction)
-        present(alert, animated: true)
     }
-}
 
-extension PlantDetailViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let image = info[.originalImage] as? UIImage else { return }
-        self.plantImageView.image = image
-        
-        dismiss(animated: true)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true)
-    }
-}
--- ios-WaterMyPlants/ios-WaterMyPlants/View Controllers/PlantListTableViewController.swift
+++ ios-WaterMyPlants/ios-WaterMyPlants/View Controllers/PlantListTableViewController.swift
@@ -1,135 +1,137 @@
 //
 //  PlantListTableViewController.swift
-//  waterMyPlantTemp
+//  ios-WaterMyPlants
 //
-//  Created by Michael Flowers on 10/18/19.
-//  Copyright © 2019 Michael Flowers. All rights reserved.
+//  Created by Michael Flowers on 10/23/19.
+//  Copyright © 2019 LambdaSchool. All rights reserved.
 //
 
 import UIKit
 import CoreData
 class PlantListTableViewController: UITableViewController {
-//    var owner: Owner?
+
+    //MARK: - Properties
     lazy var fetchedResultsController: NSFetchedResultsController<Plant> = {
-        let fetchRequest: NSFetchRequest<Plant> = Plant.fetchRequest()
-        let sortDiscriptors = NSSortDescriptor(key: "schedule", ascending: true)
-        fetchRequest.sortDescriptors = [sortDiscriptors]
-        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.shared.mainContext, sectionNameKeyPath: nil, cacheName: nil)
-        frc.delegate = self
-        
-        do {
-            try frc.performFetch()
-        } catch {
-            print("Error performing FRC fetchRequest in: \(#function)/// \(error.localizedDescription)")
-        }
-        return frc
-    }()
+           let fetchRequest: NSFetchRequest<Plant> = Plant.fetchRequest()
+           let sortDiscriptors = NSSortDescriptor(key: "schedule", ascending: true)
+           fetchRequest.sortDescriptors = [sortDiscriptors]
+           let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.shared.mainContext, sectionNameKeyPath: nil, cacheName: nil)
+           frc.delegate = self
+           
+           do {
+               try frc.performFetch()
+           } catch {
+               print("Error performing FRC fetchRequest in: \(#function)/// \(error.localizedDescription)")
+           }
+           return frc
+       }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    
+
     // MARK: - Table view data source
-    //
-    //    override func numberOfSections(in tableView: UITableView) -> Int {
-    //        // #warning Incomplete implementation, return the number of sections
-    //        return 0
-    //    }
-    
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return fetchedResultsController.fetchedObjects?.count ?? 1
     }
-    
+
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "plantCell", for: indexPath) as! PlantCellTableViewCell
-//        let owner = fetchedResultsController.object(at: indexPath)
-       // guard let plant = owner.plants?[indexPath.row] as? Plant else { return UITableViewCell() }
         let plant = fetchedResultsController.object(at: indexPath)
+        // Configure the cell...
         cell.plant = plant
-        
+
         return cell
     }
-    
-    
+  
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-//            guard let plantToDelete = fetchedResultsController.object(at: indexPath).plants?[indexPath.row] as? Plant else { return  }
-            let plantToDelete = fetchedResultsController.object(at: indexPath)
-            PlantController.delete(plant: plantToDelete)
+             let plantToDelete = fetchedResultsController.object(at: indexPath)
+                PlantController.delete(plant: plantToDelete)
         }
     }
-    
+
+   
     // MARK: - Navigation
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "cellSegue" {
-            guard let destination = segue.destination as? PlantDetailViewController, let indexPath = tableView.indexPathForSelectedRow /* let plant = fetchedResultsController.object(at: indexPath).plants?[indexPath.row] as? Plant */ else { return }
-            let plant = fetchedResultsController.object(at: indexPath)
-            destination.plant = plant
-        } else if segue.identifier == "addSegue" {
-            guard let destination = segue.destination as? PlantDetailViewController, let indexPath = tableView.indexPathForSelectedRow else { return }
-//            let owner = fetchedResultsController.object(at: indexPath)
-             let plant = fetchedResultsController.object(at: indexPath)
-            destination.user = plant.user
-//            self.owner = plant.owner
-        } else if segue.identifier == "settingSegue"{
-            guard let destination = segue.destination as? SettingsViewController  else { return }
-            let user = User(username: "testUSER", phoneNumber: "4304003940", password: "password")
-            destination.user = user
+         if segue.identifier == "cellSegue" {
+                    guard let destination = segue.destination as? PlantDetailViewController, let indexPath = tableView.indexPathForSelectedRow /* let plant = fetchedResultsController.object(at: indexPath).plants?[indexPath.row] as? Plant */ else { return }
+                    let plant = fetchedResultsController.object(at: indexPath)
+                    destination.plant = plant
+                } else if segue.identifier == "addSegue" {
+                    guard let destination = segue.destination as? PlantDetailViewController, let indexPath = tableView.indexPathForSelectedRow else { return }
+        //            let owner = fetchedResultsController.object(at: indexPath)
+                     let plant = fetchedResultsController.object(at: indexPath)
+                    destination.user = plant.user
+        //            self.owner = plant.owner
+                }
+    }
+  
+
+}
+
+extension PlantListTableViewController: NSFetchedResultsControllerDelegate {
+    //MARK: - NSFetchControllerResults
+ 
+        //tell the tableView I'm about to do a bunch of stuff
+        func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            tableView.beginUpdates()
+        }
+        //tell tableView I finished doing my stuff, you do your thing
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            tableView.endUpdates()
+        }
+        //I just created,read, update or deleted something
+        func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                        didChange anObject: Any,
+                        at indexPath: IndexPath?,
+                        for type: NSFetchedResultsChangeType,
+                        newIndexPath: IndexPath?) {
+            switch type {
+            case .insert:
+                guard let newIndexPath = newIndexPath else {return}
+                tableView.insertRows(at: [newIndexPath ], with: .automatic)
+            case .delete:
+                guard let oldIndexPath = indexPath else {return}
+                tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            case .update:
+                guard let oldIndexPath = indexPath else {return}
+                tableView.reloadRows(at: [oldIndexPath], with: .automatic)
+            case .move:
+                guard let oldIndexPath = indexPath,
+                    let newIndexPath = indexPath else {return}
+                tableView.moveRow(at: oldIndexPath, to: newIndexPath)
+            @unknown default:
+                fatalError()
+            }
         }
         
-    }
-    
-    
-}
-extension PlantListTableViewController: NSFetchedResultsControllerDelegate {
-    //will tell the tableViewController get ready to do something.
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-                    didChange anObject: Any,
-                    at indexPath: IndexPath?,
-                    for type: NSFetchedResultsChangeType,
-                    newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            //there was a new entry so now we need to make a new cell.
-            guard let newIndexPath = newIndexPath else {return}
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
-        case .delete:
-            guard let indexPath = indexPath else {return}
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        case .move:
-            guard let indexPath = indexPath, let newIndexpath = newIndexPath else {return}
-            tableView.moveRow(at: indexPath, to: newIndexpath)
-        case .update:
-            guard let indexPath = indexPath else {return}
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        @unknown default:
-            fatalError()
-        }
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
+        //i just changed the  number of sections
+        //why is this important?
+        func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                        didChange sectionInfo: NSFetchedResultsSectionInfo,
+                        atSectionIndex sectionIndex: Int,
+                        for type: NSFetchedResultsChangeType) {
             let indexSet = IndexSet(integer: sectionIndex)
-            tableView.insertSections(indexSet, with: .automatic)
-        case .delete:
-            let indexSSet = IndexSet(integer: sectionIndex)
-            tableView.deleteSections(indexSSet, with: .automatic)
-        default:
-            break
+            switch type {
+            case .insert:
+                tableView.insertSections(indexSet, with: .automatic)
+            case .delete:
+                tableView.deleteSections(indexSet, with: .automatic)
+            default:
+                return
+            }
         }
-    }
+
 }
