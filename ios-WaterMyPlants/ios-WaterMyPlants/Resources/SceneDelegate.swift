063a67bbe2b469805ebc07bc06360b7f954d9a7b
Michael R.F <mrflowers22@uwalumni.com>
2019-10-23 17:49:00 -0600

    added a scenedelegate.swift file

---
+++ ios-WaterMyPlants/ios-WaterMyPlants/Resources/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  ios-WaterMyPlants
+//
+//  Created by Michael Flowers on 10/23/19.
+//  Copyright © 2019 LambdaSchool. All rights reserved.
+//
+
+import Foundation
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+
+
+}
